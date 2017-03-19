eb1e443e2c104e7f44b7189a1e7901d8647ff0a4
YousefMohsen <yousinho7@live.dk>
2017-03-19 10:22:46 +0100

    erge

--- CA2/src/main/java/entity/Hobby.java
+++ CA2/src/main/java/entity/Hobby.java
@@ -11,17 +11,32 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
 
 /**
  *
- * @author Janus
+ * @author Yousinho
  */
 @Entity
 public class Hobby implements Serializable {
+
+    
+
+
+   
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+       
+    private String name;
+    
+    private String description;
+    
+        
+    @ManyToMany(mappedBy = "hobbies")
+    private List<Person> persons;
 
     public Hobby() {
     }
@@ -30,19 +45,7 @@ public class Hobby implements Serializable {
         this.name = name;
         this.description = description;
     }
-    
 
-    @ManyToMany(mappedBy = "hobbies")
-    private List<Person> persons;
-
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    
-    private String name;
-    
-    private String description;
 
     public Integer getId() {
         return id;
